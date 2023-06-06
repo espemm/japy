77b3d0e010defcea3d3d099d445db7fe5e367389
espemm <espemm@gmail.com>
2023-06-06 19:54:30 +0200

    Add files via upload

---
+++ ordinaria/Ejercicio1/main.py
@@ -0,0 +1,3 @@
+print(6*"--")
+print("EJERCICIO1")
+print(6*"--")
---
+++ ordinaria/Ejercicio2/src/com/ieseljust/Main.java
@@ -0,0 +1,11 @@
+package com.ieseljust;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("--".repeat(6));
+        System.out.println("EJERCICIO2");
+        System.out.println("--".repeat(6));
+    }
+}
